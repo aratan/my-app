a5eddb48e96429c61b2fecfe4bc8efb91f9b3571
victor <victor.arbiol@gmail.com>
2024-08-06 11:54:20 +0200

    first commit

---
+++ src/routes/Footer.svelte
@@ -0,0 +1,39 @@
+<script>
+	import { page } from '$app/stores';
+
+	
+</script>
+
+<footer>
+	<div class="corner">
+	pie de pagina
+	</div>
+
+	
+
+	<div class="corner">
+		<a href="https://github.com/sveltejs/kit">
+			
+		</a>
+	</div>
+</footer>
+
+<style>
+	footer {
+		display: flex;
+		justify-content: center; /* Centra el contenido horizontalmente */
+	}
+
+	.corner {
+		width: 3em;
+		height: 3em;
+	}
+
+	.corner a {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		width: 100%;
+		height: 100%;
+	}
+</style>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,20 @@
+<svelte:head>
+	<title>Nosotros</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>Anuncios y Formacion</h1>
+
+	<p>
+		Cursos, anuncios y formación
+	</p>
+
+	<pre>Parroquias por cercania</pre>
+
+	<p>
+		Te buscamos actividades de oracion.
+	</p>
+
+	
+</div>
