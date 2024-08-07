0ad3f0e66ee95c411e6ffc61d18838d59641bf84
victor <victor.arbiol@gmail.com>
2024-08-07 12:50:57 +0200

    mapa

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,56 +1,52 @@
 <script>
-	
-
-
-</script>
-
-<svelte:head>
+	import Map from './Map.svelte';
+  </script>
+  
+  <svelte:head>
 	<title>Home</title>
 	<meta name="description" content="Svelte demo app" />
-</svelte:head>
-
-<section>
+  </svelte:head>
+  
+  <section>
 	<h1>
-		<span class="welcome">
-			
-		</span>
-
-		cercanas<br />5 km
+	  <span class="welcome"></span>
+	  cercanas<br />5 km
 	</h1>
-
-	<h2>
-		Parroquias <strong>Catolicas</strong>
-	</h2>
-
 	
-</section>
-
-<style>
+	<Map />
+	
+	<h2>
+	  Parroquias <strong>Catolicas</strong>
+	</h2>
+  </section>
+  
+  <style>
 	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
+	  display: flex;
+	  flex-direction: column;
+	  justify-content: center;
+	  align-items: center;
+	  flex: 0.6;
 	}
-
+  
 	h1 {
-		width: 100%;
+	  width: 100%;
 	}
-
+  
 	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
+	  display: block;
+	  position: relative;
+	  width: 100%;
+	  height: 0;
+	  padding: 0 0 calc(100% * 495 / 2048) 0;
 	}
-
+  
 	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
+	  position: absolute;
+	  width: 100%;
+	  height: 100%;
+	  top: 0;
+	  display: block;
 	}
-</style>
+  </style>
+  
---
+++ src/routes/Map.svelte
@@ -0,0 +1,24 @@
+<script>
+    import { onMount } from 'svelte';
+    import * as L from 'leaflet';
+    import 'leaflet/dist/leaflet.css';
+  
+    let map;
+  
+    onMount(() => {
+      map = L.map('map').setView([40.29753, -3.83810], 18);
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      }).addTo(map);
+    });
+  </script>
+  
+  <style>
+    #map {
+      height: 400px;
+      width: 100%;
+    }
+  </style>
+  
+  <div id="map"></div>
+  
