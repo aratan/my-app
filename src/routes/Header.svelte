35d8094661df4813c49c4bd3f0c38242d5c3b7ef
victor <victor.arbiol@gmail.com>
2024-08-07 12:46:33 +0200

    mapa simple

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -14,6 +14,8 @@
 	<nav>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
 			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
+			
+			
 		</svg>
 		<ul>
 			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
