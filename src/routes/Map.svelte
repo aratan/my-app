0991447b0781acbba104e4256e024e449f4d418b
victor <victor.arbiol@gmail.com>
2024-08-08 09:25:01 +0200

    fix 500

--- src/routes/Map.svelte
+++ src/routes/Map.svelte
@@ -1,24 +1,27 @@
 <script>
-    import { onMount } from 'svelte';
-    import * as L from 'leaflet';
-    import 'leaflet/dist/leaflet.css';
-  
-    let map;
-  
-    onMount(() => {
-      map = L.map('map').setView([40.29753, -3.83810], 18);
-      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      }).addTo(map);
-    });
-  </script>
-  
-  <style>
-    #map {
+  import { onMount } from 'svelte';
+  import { browser } from '$app/environment';
+
+  let map;
+
+  onMount(async () => {
+      if (browser) {
+          const L = await import('leaflet');
+          await import('leaflet/dist/leaflet.css');
+
+          map = L.map('map').setView([40.29753, -3.83810], 18);
+          L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+              attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          }).addTo(map);
+      }
+  });
+</script>
+
+<style>
+  #map {
       height: 400px;
       width: 100%;
-    }
-  </style>
-  
-  <div id="map"></div>
-  
+  }
+</style>
+
+<div id="map"></div>
